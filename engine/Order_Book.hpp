90836b81a77bb1d3362082a07ca65e8d72fcd661
AadityaKarki <aaditya.karki01@gmail.com>
2026-02-17 12:09:49 +0530

    Trade class and cancelOrder function

--- engine/Order_Book.hpp
+++ engine/Order_Book.hpp
@@ -10,18 +10,38 @@ class Order_Book{
     public:
 
     private:
-    vector<Order> orders_;
     map<Price, std::list<Orderptr>, std::greater<Price>>bidList_;
     map<Price, std::list<Orderptr>, std::less<Price>>askList_;
     unordered_map<Order_Id, Orderptr> orderMap_;
+    Price bestBidPrice_;
+    Price bestAskPrice_;
+    Price lastTradePrice_;
     void addOrder(Order& order){
-        orders_.push_back(order);
-        orderMap_.insert({order.getOrderId(), &orders_.back()});
+        orderMap_.insert({order.getOrderId(), &order});
         if(order.getSide() == Side::Buy){
-            bidList_.insert({order.getPrice(), &orders_.back()});
+            bidList_.insert({order.getPrice(), &order});
         }
         else{
-            askList_.insert({order.getPrice(), &orders_.back()});
+            askList_.insert({order.getPrice(), &order});
+        }
+    }
+    void cancelOrder(Order_Id order_id){
+        auto it = orderMap_.find(order_id);
+        if(it != orderMap_.end()){
+            Orderptr order = it->second;
+            if(order->isFilled()){
+                throw std::logic_error("Order is already filled");
+            }
+            else{
+                if(order->getSide() == Side::Buy){
+                    bidList_.erase(order->getPrice());
+                }
+                else{
+                    askList_.erase(order->getPrice());
+                }
+                orderMap_.erase(order_id);
+               
+            }
         }
     }
 }
